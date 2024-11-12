b4ce93caf1850d46328b3df40b8558b1a0929cd9
Matrix2121 <vasko10.vs38@gmail.com>
2024-11-12 17:10:05 +0200

    Menu class

--- src/menu.h
+++ src/menu.h
@@ -2,8 +2,13 @@
 #include <string>
 
 class Menu{
+<<<<<<< HEAD
     void mainMenu();
     void displayMenu();
     void createMenu();
     void editMenu();
+=======
+    
+
+>>>>>>> 896f384 (menu class)
 };
