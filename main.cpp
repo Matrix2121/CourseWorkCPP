560898cf198ea9a39bdf7edd3708fe9145751d9a
Matrix2121 <vasko10.vs38@gmail.com>
2024-11-14 23:20:49 +0200

    Cleared Main class

--- main.cpp
+++ main.cpp
@@ -4,15 +4,4 @@
 
 int main(){
     Menu::mainMenu();
-
-
-
-    
-    Route r1 = Route({"carigradsko", "luvov most", "zona b5"}, 10.1, 3);
-    std::cout << r1 << std::endl;
-    Car c1 = Car("Ford", "Fiesta", 2005, 5, 1300, 7.1);
-    std::cout << c1 << std::endl;
-
-    
-
 }
